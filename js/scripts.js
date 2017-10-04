f5a67f46e63f2c9a7403a49eccf002746b40961c
koscielniakolaewa <31932678+koscielniakolaewa@users.noreply.github.com>
2017-10-04 12:42:46 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
-var drawTree = 7
-for (i=1;i<=drawTree;i++) {
-    for (star=1;star<=i;star++)
-    {
-    	document.write("*");
-    }
-    document.write("<br>");
+function drawTree(h) {
+for (i=1;i<=h;i++) {
+for (star=1;star<=i;star++)
+{
+document.write("*");
 }
+document.write("<br>");
+}
+}
+
+drawTree(11);
